ec223b71e8de5041a4b8cddb78181d2575a2bb1e
GijsVliegen <r0810400@kuleuven.be>
2021-12-07 15:59:30 +0100

    isClose verbeterd

--- rotations.jl
+++ rotations.jl
@@ -4,14 +4,32 @@ struct quaternion
     k
     angle
 end
-function isClose(first, other, rtol, atol, equal_nan = True)
-    if isnan(first)
-        if isnan(other)
-            return equal_nan
-        return false
+
+function getComponents(rot ::quaternion)
+    return [rot.i, rot.j, rot.k, rot.angle]
+end
+
+function toQuaternion(rot ::quaternion)
+    return rot
+end
+
+#in tegenstelling tot in damask wordt er niet rekening gehouden met NaN values
+#dit doen we omdat het niet is ingebouwd in isapprox()
+
+function isClose(first ::quaternion, other ::quaternion, rtol, atol)
+    fcom = getComponents(toQuaternion(first))
+    scom = getComponents(toQuaternion(other))
+    for i in 1:4
+        if !isapprox(fcom[i], scom[i], rtol = rtol , atol = atol)
+            return false
         end
-    end    
-    fQu = toQuaternion(first)
-    sQu = toQuaternion(other)
-    return absolute(fQu - sQu) <= (atol + rtol * absolute(b))
+    end
+    return true
+end    
+quOne = quaternion(1, 1, 1, 0)
+quTwo = quaternion(1+1e-1, 1, 1, 0)
+if isClose(quOne, quTwo, 0.01, 0.01)
+    println("yeet")
+else
+    println("niet yeet")
 end    
