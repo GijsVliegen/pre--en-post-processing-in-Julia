5337ba7f2e13ace3685ef029bee4361a992cdcf7
Tim Olde <tim.olde@student.kuleuven.be>
2022-03-16 13:59:51 +0100

    functienamen aangepast

--- test.jl
+++ test.jl
@@ -2,42 +2,17 @@ using Test
 using Random
 include("rotations.jl")
 
-# quOne = quaternion(1, 1, 1, 0)
-# quTwo = quaternion(1+1e-1, 1, 1, 0)
-# if isClose(quOne, quTwo, 0.01, 0.01)
-#     println("yeet")
-# else
-#     println("niet yeet")
-# end
-#
-# eA = eulerAngle(1, 1, 1)
-# rotMatrix = eulerAngleToRotationMatrix(eA)
-# print(rotMatrix)
-
 # generate a array of N uniformly distributed random quaternions: http://planning.cs.uiuc.edu/node198.html
 N=1000
-quaternions = quaternion[]
-for i = 1:N
-    u1 = rand()
-    u2 = rand()
-    u3 = rand()
-    h = quaternion(sqrt(1-u1)*sin(2*pi*u2), sqrt(1-u1)*cos(2*pi*u2), sqrt(u1)*sin(2*pi*u3), sqrt(u1)*cos(2*pi*u3))
-    push!(quaternions, copy(h))
-end
-
-eulerAngles = eulerAngle[]
-for i = 1:N
-
-end
-
+quaternions = from_random(N)
 
 @testset "rotations.jl" begin
     # conversion between eulerAngle and quaternion
     @test begin
                 result = true
                 for q in quaternions
-                    e = toEulerAngle(q)
-                    q1 = toQuaternion(e)
+                    e = as_euler_angle(q)
+                    q1 = from_Euler_angles(e)
                     if !isClose(q,q1,10^(-13),10^(-13))
                         result = false
                         break
@@ -50,8 +25,8 @@ end
     @test begin
                 result = true
                 for q in quaternions
-                    m = toRotationMatrix(q)
-                    q1 = toQuaternion(m)
+                    m = as_matrix(q)
+                    q1 = from_matrix(m)
                     if !isClose(q,q1,10^(-13),10^(-13))
                         result = false
                         break
@@ -64,8 +39,8 @@ end
     @test begin
                 result = true
                 for q in quaternions
-                    a = toAxisAngle(q)
-                    q1 = toQuaternion(a)
+                    a = as_axis_angle(q)
+                    q1 = from_axis_angle(a)
                     if !isClose(q,q1,10^(-13),10^(-13))
                         result = false
                         break
@@ -78,8 +53,8 @@ end
     @test begin
                 result = true
                 for q in quaternions
-                    r = toRodriguesFrank(q)
-                    q1 = toQuaternion(r)
+                    r = as_rodriguesfrank(q)
+                    q1 = from_rodriguesfrank(r)
                     if !isClose(q,q1,10^(-13),10^(-13))
                         result = false
                         break
