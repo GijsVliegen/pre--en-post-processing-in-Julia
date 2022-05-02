769c3660bf9b58787146ddd44acd3eda4293f9e0
r0810400 <r0810400@kuleuven.be>
2022-05-02 21:51:25 +0200

    forgot to include the files

---
+++ juliaTimingToExcel.py
@@ -0,0 +1,52 @@
+
+from openpyxl import Workbook, workbook, load_workbook
+import random
+workbook = load_workbook(filename="metingen WV py-rot-fast.xlsx")
+workbook.sheetnames
+['Sheet 1']
+sheet = workbook.active
+
+with open('/home/gijs/free/programmeren/julia/WV/rotations.jl/juliaTimingResults', 'r') as file:
+    data = file.readlines()
+
+def main():
+    sheet["A1"] = "OM"
+    sheet["B1"] = "EU"
+    sheet["C1"] = "AX"
+    sheet["D1"] = "HO"
+    sheet["E1"] = "RO"
+    sheet["F1"] = "n"
+    sheet["G1"] = "NOT all times are in ms"
+    sheet["H1"] = "eenheid om"
+    sheet["I1"] = "eenheid eu"
+    sheet["J1"] = "eenheid ax"
+    sheet["K1"] = "eenheid ho"
+    sheet["L1"] = "eenheid ro"
+#altijd eerst om, dan eu, dan ax, dan ho, dan ro
+main()
+
+rowCounter = 2
+columnCounter = 0
+columnString = "ABCDEF"
+columnStringEenheid = "HIJKL"
+
+for i in data:
+    print(i)
+    if i.startswith("voor"):
+        n = i[9:-1] #nakijken
+        sheet[columnString[5]+str(rowCounter)] = str(n)
+    if i.startswith("  "):
+        end = i.find("(") - 4 #nakijken
+        res = i[2: end]
+        eenheid = i[end+1:end+3]
+        print("res = ", res)
+        print("eenheid = ", eenheid)
+        sheet[columnString[columnCounter]+str(rowCounter)] = str(res)
+        sheet[columnStringEenheid[columnCounter]+str(rowCounter)] = str(eenheid)
+        columnCounter += 1
+    if columnCounter == 5:
+        columnCounter = 0
+        rowCounter += 1
+
+rand = random.randrange(1, 100000)
+workbook.save("meting WV jl.xlsx")
---
+++ rotationtest_timing.py
@@ -0,0 +1,119 @@
+from functools import total_ordering
+import damask, time
+from openpyxl import Workbook, workbook, load_workbook
+import rotationtest
+import random
+
+workbook = load_workbook(filename="metingen WV py-rot-fast.xlsx")
+workbook.sheetnames
+['Sheet 1']
+sheet = workbook.active
+
+
+#time
+def operationEu(a, l):
+    totaltime = 0
+    for i in range(l):
+        processST = time.process_time()
+        for k in a:
+            i = k.quaternion
+            b = rotationtest.qu2eu(i)
+            c = rotationtest.eu2qu(b)
+        processET = time.process_time()
+        totaltime += processET - processST
+    print(totaltime/l * 10**3)
+    return(totaltime/l* 10**3)
+
+def operationOm(a, l):
+    totaltime = 0
+    for i in range(l):
+        processST = time.process_time()
+        for k in a:
+            i = k.quaternion
+            b = rotationtest.qu2om(i)
+            c = rotationtest.om2qu(b)
+        processET = time.process_time()
+        totaltime += processET - processST
+    print(totaltime/l * 10**3)
+    return(totaltime/l* 10**3)
+
+def operationAx(a, l):
+    totaltime = 0
+    for i in range(l):
+        processST = time.process_time()
+        for k in a:
+            i = k.quaternion
+            b = rotationtest.qu2ax(i)
+            c = rotationtest.ax2qu(b)
+        processET = time.process_time()
+        totaltime += processET - processST
+    print(totaltime/l * 10**3)
+    return(totaltime/l* 10**3)
+
+def operationHo(a, l):
+    totaltime = 0
+    for i in range(l):
+        processST = time.process_time()
+        for k in a:
+            i = k.quaternion
+            b = rotationtest.qu2ho(i)
+            c = rotationtest.ho2ax(b)
+            d = rotationtest.ax2qu(c)
+        processET = time.process_time()
+        totaltime += processET - processST
+    print(totaltime/l * 10**3)
+    return(totaltime/l* 10**3)
+
+def operationRo(a, l):
+    totaltime = 0
+    for i in range(l):
+        processST = time.process_time()
+        for k in a:
+            i = k.quaternion
+            b = rotationtest.qu2ro(i)
+            c = rotationtest.ho2ax(b)
+            d = rotationtest.ax2qu(c)
+        processET = time.process_time()
+        totaltime += processET - processST
+    print(totaltime/l * 10**3)
+    return(totaltime/l* 10**3)
+
+#a zijn de rotaties, l is aantal herhalingen van de test
+#a zijn de rotaties, l is aantal herhalingen van de test
+def oneTest(a, l, row):
+    row = str(row)
+    result = operationEu(a, l)
+    sheet["A"+row] = str(result)
+    result = operationOm(a, l)
+    sheet["B"+row] = str(result)
+    result = operationAx(a, l)
+    sheet["C"+row] = str(result)
+    result = operationHo(a, l)
+    sheet["D"+row] = str(result)
+    result = operationRo(a, l)
+    sheet["E"+row] = str(result)
+
+def longTestRun():
+    print("EU - OM - AX - HO - RO, all times in ms")
+    min = 1
+    max = 17
+    for i in range (min,max):
+        sheet["F"+str(2+i-min)] = i
+        l = 50
+        n = 2**i
+        print(n)
+        a = damask.Rotation.from_random(n)
+        oneTest(a, l, 2+i-min)
+
+def main():
+    sheet["A1"] = "EU"
+    sheet["B1"] = "OM"
+    sheet["C1"] = "AX"
+    sheet["D1"] = "HO"
+    sheet["E1"] = "RO"
+    sheet["F1"] = "n"
+    sheet["G1"] = "all times are in ms"
+main()
+longTestRun()
+nr = random.randrange(1, 100000)
+workbook.save("meting WV py test.xlsx")
